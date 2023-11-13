6138c613d085a33879e302190dddec6c91ec4f28
sheyerdahl <97766761+sheyerdahl@users.noreply.github.com>
2023-11-12 16:33:26 -0800

    Added more to info text

--- src/containers/InfoMenu/InfoMenu.tsx
+++ src/containers/InfoMenu/InfoMenu.tsx
@@ -3,7 +3,7 @@ function InfoMenu(): JSX.Element {
     return (
             <div className="Main-content ml7">
                 <p className="light-yellow">How Mine-Sweeper works:</p>
-                <p>Your goal is to reveal all tiles without a mine. Tiles display how many mines are around it, including diagonally. Place flags on mines to remember them, you can't activate tiles with flags.</p>
+                <p>Your goal is to reveal all tiles without a mine. Tiles display how many mines are around it, including diagonally. Place flags on mines to remember them, you can't activate tiles with flags. Lastly, have fun!</p>
                 <p className="light-blue">Controls:</p>
                 <p className="ma1">LMB: Reveal Tile</p>
                 <p className="ma1">RMB: Place Flag</p>
